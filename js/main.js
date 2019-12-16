c9ce804a0362bc0fd3b38214757a7ca5280814cf
maddieritter <maddieritter46@gmail.com>
2019-12-15 21:27:53 -0600

    Final grade submit
    Added in Jquery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+// A $( document ).ready() block.
 $( document ).ready(function() {
-  console.log( "ready!" );
 
+  $("#gallery, #button").click(function () {
+    alert("Page under construction");
+  });
 
+  $("#downarrow").click(function(){
+    $("#phone, #email").slideDown(1000);
+  });
 
-
-
-
-
+  setTimeout(function () {
+    $('#title').addClass('text-warning');
+    }, 2000);
 });
+
